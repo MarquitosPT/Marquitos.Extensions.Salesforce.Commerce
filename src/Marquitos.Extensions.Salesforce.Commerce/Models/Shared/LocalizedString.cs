cf5873cadba184aa9f2619cad132aabd67b9adb8
Marcos Gomes <marquitospt@hotmail.com>
2023-11-01 19:18:32 +0000

    Updated documentation

--- src/Marquitos.Extensions.Salesforce.Commerce/Models/Shared/LocalizedString.cs
+++ src/Marquitos.Extensions.Salesforce.Commerce/Models/Shared/LocalizedString.cs
@@ -3,23 +3,40 @@ using System.Xml.Serialization;
 
 namespace Marquitos.Salesforce.Commerce.Models.Shared
 {
+    /// <summary>
+    /// Represents a localized string
+    /// </summary>
     public class LocalizedString
     {
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public LocalizedString()
         {
             Value = string.Empty;
             Language = Language.Default;
         }
 
+        /// <summary>
+        /// Creates a new instance using the provided parameters
+        /// </summary>
+        /// <param name="value">The string value</param>
+        /// <param name="language">The language of the string value</param>
         public LocalizedString(string value, Language language = Language.Default)
         {
             Value = value;
             Language = language;
         }
 
+        /// <summary>
+        /// The string value
+        /// </summary>
         [XmlText]
         public string Value { get; set; }
 
+        /// <summary>
+        /// The language of the string value
+        /// </summary>
         [XmlAttribute("xml:lang")]
         public Language Language { get; set; }
     }
--- src/Marquitos.Extensions.Salesforce.Commerce/Models/Shared/LocalizedStrings.cs
+++ src/Marquitos.Extensions.Salesforce.Commerce/Models/Shared/LocalizedStrings.cs
@@ -2,21 +2,40 @@
 
 namespace Marquitos.Salesforce.Commerce.Models.Shared
 {
+    /// <summary>
+    /// Represents a list of localized strings
+    /// </summary>
     public class LocalizedStrings : List<LocalizedString>
     {
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public LocalizedStrings()
         { }
 
+        /// <summary>
+        /// Creates a new instance with a single localized string value
+        /// </summary>
+        /// <param name="value">The string value</param>
+        /// <param name="language">The language of the string value</param>
         public LocalizedStrings(string value, Language language = Language.Default)
         {
             Add(new LocalizedString(value, language));
         }
 
+        /// <summary>
+        /// Creates a new instance with a single localized string value
+        /// </summary>
+        /// <param name="value">The localized string object</param>
         public LocalizedStrings(LocalizedString value)
         {
             Add(value);
         }
 
+        /// <summary>
+        /// Creates a new instance with the provided localized string values
+        /// </summary>
+        /// <param name="value">List of localized string objects</param>
         public LocalizedStrings(params LocalizedString[] value)
         {
             AddRange(value);
